21906bfac943e0cfbfabb4f1da65d60e0e3bfd75
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-04 13:36:58 -0300

    port update

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
+const { PGPORT } = process.env
+const PORT = process.env.PGPORT || 3001
 
 // Syncing all the models at once.
 conn.sync().then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001') // eslint-disable-line no-console
+  server.listen(`${PORT}`, () => {
+    console.log(`server running on port ${PORT}`)
   })
 })
